e424a8a87a7cbdea82d1c95772d54198fe56ddbd
Edi Wang <Edi.Wang@outlook.com>
2015-11-11 18:14:41 +0800

    add BitmapImageConverter

---
+++ Edi.UWP.Helpers/Edi.UWP.Helpers/Converters/BitmapImageConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Edi.UWP.Helpers.Converters
+{
+    public class BitmapImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var s = value as string;
+            if (s != null)
+            {
+                return new BitmapImage(new Uri(s, UriKind.RelativeOrAbsolute));
+            }
+
+            var uri = value as Uri;
+            if (uri != null)
+            {
+                return new BitmapImage(uri);
+            }
+
+            throw new NotSupportedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
