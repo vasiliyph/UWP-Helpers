85369854b576cb83b508a68a5c971b5092bdf335
h82258652 <842053625@qq.com>
2015-11-11 14:46:34 +0800

    Update CollectionExtensions.cs

--- Edi.UWP.Helpers/Edi.UWP.Helpers/CollectionExtensions.cs
+++ Edi.UWP.Helpers/Edi.UWP.Helpers/CollectionExtensions.cs
@@ -11,12 +11,7 @@ namespace Edi.UWP.Helpers
     {
         public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> coll)
         {
-            var c = new ObservableCollection<T>();
-            foreach (var e in coll)
-            {
-                c.Add(e);
-            }
-            return c;
+            return new ObservableCollection<T>(coll);
         }
     }
 }
