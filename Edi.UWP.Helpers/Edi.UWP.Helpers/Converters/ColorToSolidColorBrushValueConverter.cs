4d18d9cb76f0368592c3bb0453ab22a88f90b373
Edi Wang <Edi.Wang@outlook.com>
2015-11-11 18:06:43 +0800

    seperate color converters

---
+++ Edi.UWP.Helpers/Edi.UWP.Helpers/Converters/ColorToSolidColorBrushValueConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace Edi.UWP.Helpers.Converters
+{
+    public class ColorToSolidColorBrushValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (null == value)
+            {
+                return null;
+            }
+
+            if (value is Color)
+            {
+                var color = (Color)value;
+                return new SolidColorBrush(color);
+            }
+
+            var type = value.GetType();
+            throw new InvalidOperationException($"Unsupported type [{type.Name}]");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
