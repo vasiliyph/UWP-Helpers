8832924881b5d6a22a0b40e66225ef4360db690e
Edi Wang <Edi.Wang@outlook.com>
2015-11-13 13:40:33 +0800

    add bitmapextension

---
+++ Edi.UWP.Helpers/Edi.UWP.Helpers/BitmapExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Edi.UWP.Helpers
+{
+    public static class BitmapExtensions
+    {
+        public static byte[] ToByteArray(WriteableBitmap bitmap)
+        {
+            return bitmap.PixelBuffer.ToArray();
+        }
+
+        public static Stream ToStream(WriteableBitmap bitmap)
+        {
+            return bitmap.PixelBuffer.AsStream();
+        }
+    }
+}
