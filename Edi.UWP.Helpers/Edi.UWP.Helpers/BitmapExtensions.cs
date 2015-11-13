14f04c5fa501d1ba668b487e204f07754ef62007
Edi Wang <Edi.Wang@outlook.com>
2015-11-13 13:47:40 +0800

    add save method

--- Edi.UWP.Helpers/Edi.UWP.Helpers/BitmapExtensions.cs
+++ Edi.UWP.Helpers/Edi.UWP.Helpers/BitmapExtensions.cs
@@ -5,20 +5,58 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Provider;
 using Windows.UI.Xaml.Media.Imaging;
 
 namespace Edi.UWP.Helpers
 {
     public static class BitmapExtensions
     {
-        public static byte[] ToByteArray(WriteableBitmap bitmap)
+        public static byte[] ToByteArray(this WriteableBitmap bitmap)
         {
             return bitmap.PixelBuffer.ToArray();
         }
 
-        public static Stream ToStream(WriteableBitmap bitmap)
+        public static Stream ToStream(this WriteableBitmap bitmap)
         {
             return bitmap.PixelBuffer.AsStream();
+        }
+
+        public static async Task<FileUpdateStatus> SaveToPngImage(this WriteableBitmap bitmap, PickerLocationId location, string fileName)
+        {
+            var savePicker = new FileSavePicker
+            {
+                SuggestedStartLocation = location
+            };
+            savePicker.FileTypeChoices.Add("Png Image", new[] { ".png" });
+            savePicker.SuggestedFileName = fileName;
+            StorageFile sFile = await savePicker.PickSaveFileAsync();
+            if (sFile != null)
+            {
+                CachedFileManager.DeferUpdates(sFile);
+
+                using (var fileStream = await sFile.OpenAsync(FileAccessMode.ReadWrite))
+                {
+                    BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.PngEncoderId, fileStream);
+                    Stream pixelStream = bitmap.PixelBuffer.AsStream();
+                    byte[] pixels = new byte[pixelStream.Length];
+                    await pixelStream.ReadAsync(pixels, 0, pixels.Length);
+                    encoder.SetPixelData(BitmapPixelFormat.Bgra8, BitmapAlphaMode.Ignore,
+                              (uint)bitmap.PixelWidth,
+                              (uint)bitmap.PixelHeight,
+                              96.0,
+                              96.0,
+                              pixels);
+                    await encoder.FlushAsync();
+                }
+
+                FileUpdateStatus status = await CachedFileManager.CompleteUpdatesAsync(sFile);
+                return status;
+            }
+            return FileUpdateStatus.Failed;
         }
     }
 }
