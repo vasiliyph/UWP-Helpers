4e72db28e3042bd590ec43a6696d6b8c79e3cfa0
Edi Wang <Edi.Wang@outlook.com>
2017-07-23 16:14:49 +0800

    Add accent color sample

--- Edi.UWP.Helpers/Edi.UWP.Helpers.Sample/ViewModels/UIViewModel.cs
+++ Edi.UWP.Helpers/Edi.UWP.Helpers.Sample/ViewModels/UIViewModel.cs
@@ -1,13 +1,30 @@
 using System;
-
+using Windows.UI.Xaml.Media;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace Edi.UWP.Helpers.Sample.ViewModels
 {
     public class UIViewModel : ViewModelBase
     {
+        private SolidColorBrush _accentColor;
+
         public UIViewModel()
         {
+            CommandGetAccentColor = new RelayCommand(GetAccentColor);
+        }
+
+        public SolidColorBrush AccentColor
+        {
+            get => _accentColor;
+            set { _accentColor = value; RaisePropertyChanged(); }
+        }
+
+        public RelayCommand CommandGetAccentColor { get; set; }
+
+        private void GetAccentColor()
+        {
+            AccentColor = new SolidColorBrush(Edi.UWP.Helpers.UI.GetAccentColor());
         }
     }
 }
